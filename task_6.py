b915e06f90e306f4f9ce60a825bac7ce5ea75b49
Egorova-DK <115218492+Egorova-DK@users.noreply.github.com>
2022-10-14 20:44:46 +0300

    Лабораторная работа 3

---
+++ task_6.py
@@ -0,0 +1,10 @@
+list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+max = 0
+for i in range(len(list_numbers)):
+    if max < list_numbers[i]:
+        max = list_numbers[i]
+        max_index = i
+
+list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
+print(list_numbers)
