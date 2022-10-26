7eeead49d9db2d2a5d5546cedf4b4a093f0acdaf
Egorova-DK <115218492+Egorova-DK@users.noreply.github.com>
2022-10-26 19:48:02 +0300

    Update task3.py
    Исправила таск3, вернула по умолчанию index = None

--- Лабораторная работа 4/task3.py
+++ Лабораторная работа 4/task3.py
@@ -4,12 +4,12 @@
 # Результатом вернуть измененный список.
 # Для удаления попробуйте использовать слайсирование, чтобы разбить список на два. А потом сложить списки без удаляемого значения.
 
-def delete(list_, index=-1):
-    if index in list_:
-        return list_[:index]+list_[index+1:]
-    else:
-        return list_[:index]
+def delete(list_, index=None):
+    if index is None:
+        index = list_[-1]
+    list_ = list_[:index] + list_[index+1:]
+    return list_
 
-print(delete([0, 0, 1, 2], index=0))  # [0, 1]
-print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
-print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
+print(delete([0, 0, 1, 2], index=0))  # [0, 1, 2]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2, 3]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3, 4]
